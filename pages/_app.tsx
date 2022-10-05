692ce7b26d02121ccaef40731335f9bf68c17177
Salif Hein <salifoutefa@yahoo.fr>
2022-10-05 07:29:39 +0100

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,45 @@
+import "../styles/globals.css";
+
+import {
+  ChainInfoID,
+  WalletManagerProvider,
+  WalletType,
+} from "@josefleventon/cosmodal";
+import type { AppProps } from "next/app";
+import { FunctionComponent } from "react";
+import { GasPrice } from "@cosmjs/stargate";
+
+import { ChakraProvider } from "@chakra-ui/react";
+
+const LOCAL_STORAGE_KEY = "connectedWalletId";
+
+const MyApp: FunctionComponent<AppProps> = ({ Component, pageProps }) => (
+  <ChakraProvider>
+    <WalletManagerProvider
+      walletConnectClientMeta={{
+        name: "CosmodalExampleDApp",
+        description: "A dApp using the @noahsaso/cosmodal library.",
+        url: "https://noahsaso-cosmodal.vercel.app",
+        icons: ["https://moonphase.is/image.svg"],
+      }}
+      enabledWalletTypes={[WalletType.Keplr, WalletType.WalletConnectKeplr]}
+      renderLoader={() => <p>Loading...</p>}
+      localStorageKey={LOCAL_STORAGE_KEY}
+      defaultChainId={ChainInfoID.Stargaze1}
+      getSigningCosmWasmClientOptions={(chainInfo) => ({
+        gasPrice: GasPrice.fromString(
+          "0.0025" + chainInfo.feeCurrencies[0].coinMinimalDenom
+        ),
+      })}
+      getSigningStargateClientOptions={(chainInfo) => ({
+        gasPrice: GasPrice.fromString(
+          "0.0025" + chainInfo.feeCurrencies[0].coinMinimalDenom
+        ),
+      })}
+    >
+      <Component {...pageProps} />
+    </WalletManagerProvider>
+  </ChakraProvider>
+);
+
+export default MyApp;
