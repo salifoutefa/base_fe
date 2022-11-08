6a08484681e383755f15c3e0f138a912b1a013f8
Salif Hein <salifoutefa@yahoo.fr>
2022-11-08 06:39:39 +0100

    Format

--- pages/upload/FileHeader.tsx
+++ pages/upload/FileHeader.tsx
@@ -1,5 +1,5 @@
-import { Box, Button, GridItem, SimpleGrid } from '@chakra-ui/react';
-import React from 'react';
+import { Box, Button, GridItem, SimpleGrid } from "@chakra-ui/react";
+import React from "react";
 
 export interface FileHeaderProps {
   file: File;
@@ -9,23 +9,22 @@ export interface FileHeaderProps {
 export function FileHeader({ file, onDelete }: FileHeaderProps) {
   return (
     <Box>
-    <SimpleGrid >
-    <GridItem
-        mt={[5, null, 0]}
-        colSpan={{
-          md: 2,
-        }}
-      >
-        {file.name}
+      <SimpleGrid>
+        <GridItem
+          mt={[5, null, 0]}
+          colSpan={{
+            md: 2,
+          }}
+        >
+          {file.name}
         </GridItem>
-      
-      <GridItem >
-        <Button size="small" onClick={() => onDelete(file)}>
-          Delete
-        </Button>
-      </GridItem>
 
-    </SimpleGrid>
+        <GridItem>
+          <Button size="small" onClick={() => onDelete(file)}>
+            Delete
+          </Button>
+        </GridItem>
+      </SimpleGrid>
     </Box>
   );
 }
